3b3b9a6bb1c3155f7cb675d14479f14703e49eb7
Benis Tambe <will.jenifer@yahoo.com>
2021-01-16 20:33:57 -0500

    commit tree 4d0c5b8d8f25f39669364a5348efd5cd9b9a0e7e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+
+    return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def countdown_with_sleep(number)
+    sleep(5)
+    while number > 0
+        
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+
+    return "HAPPY NEW YEAR!"
+    
+end
+countdown_with_sleep(12)
+
